07bbcbb7377c5fe44dac103b398ed162c46305be
Vadym Kostiuk <kastillio2012@gmail.com>
2022-11-17 19:33:37 +0100

    17.11 final

---
+++ js/mobile-menu.js
@@ -0,0 +1,24 @@
+(() => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const openMenuBtn = document.querySelector('.header__mobile-menu--open');
+    const closeMenuBtn = document.querySelector('.mobile-menu__btn-close');
+
+    const toggleMenu = () => {
+        const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+        mobileMenu.classList.toggle('is-open');
+
+        const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+        bodyScrollLock[scrollLockMethod](document.body);
+    };
+
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+        if (!e.matches) return;
+        mobileMenu.classList.remove('is-open');
+        openMenuBtn.setAttribute('aria-expanded', false);
+        bodyScrollLock.enableBodyScroll(document.body);
+    });
+})();
